1b37737d0319bd2eb34d8bedbffe2d062392be36
Erkan334 <erik.abrahamsson16@hotmail.com>
2026-09-08 20:25:12 +0200

    Updated data annotations for Expense Entity

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,18 +1,25 @@
 ﻿using EcoScope.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace EcoScope.Models
 {
     public class Expense
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(200), MinLength(2)]
+        public string Title { get; set; } = null!;
+
+        [Range(1, 1000000)]
         public decimal CostAmount { get; set; }
+
         public BillingFrequency BillingFrequency { get; set; }
 
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User User { get; set; } = null!;
 
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category Category { get; set; } = null!;
     }
 }
