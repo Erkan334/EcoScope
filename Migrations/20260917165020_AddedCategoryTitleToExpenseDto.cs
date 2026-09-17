776535da4aa31b8f0c56386eb42a59341aa19f9c
Erkan334 <erik.abrahamsson16@hotmail.com>
2026-09-17 18:51:04 +0200

    Added CategoryTitle to ExpenseDto

---
+++ Migrations/20260917165020_AddedCategoryTitleToExpenseDto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoScope.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCategoryTitleToExpenseDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
